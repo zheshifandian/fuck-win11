0d3f7177c9261a1721dc184173f4a703f54cab1e
zheshifandian <sunjiazheng321521@gmail.com>
2022-07-07 12:40:32 +0800

    fix

--- start.bat
+++ start.bat
@@ -13,6 +13,8 @@ SET "sed=%~dp0bin\bin\sed\sed.exe"
 if not exist %~dp0image\install.wim ( echo install.wim doesn't exist ) & pause exit
 if not exist %~dp0image\winre.wim ( echo winre.wim doesn't exist ) & pause exit
 
+mkdir tmp
+
 call :Prepare-Addition
 call "%~dp0bin\bin\NSudo.exe" -U:T -P:E "%~dp0build.bat"
 
